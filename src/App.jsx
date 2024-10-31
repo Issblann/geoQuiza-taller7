ef638f8dcc42389ba4f90d5b71890f25bc959fc7
Issblann <anaisabelpp@outlook.com>
2024-10-30 19:55:21 -0500

    estructura repo general, componentes, estilos, layout, rutas y redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <>
+      <h1>Geo quizza</h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
