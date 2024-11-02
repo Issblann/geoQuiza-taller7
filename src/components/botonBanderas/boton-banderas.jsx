9cad813577cc3495b268a6111e5a53784128cf06
Issblann <anaisabelpp@outlook.com>
2024-11-01 19:13:50 -0500

    componente boton banderas que manejara los estados de la trivia

---
+++ src/components/botonBanderas/boton-banderas.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/botonBanderas/styles.css';
+const BotonBanderas = () => {
+  return (
+    <section className="boton-banderas__seccion">
+      <button className="button-banderas">EE.UU. ISLAS PERIFÉRICAS</button>
+
+      <button className="button-banderas">PAISES BAJOS</button>
+    </section>
+  );
+};
+
+export default BotonBanderas;
