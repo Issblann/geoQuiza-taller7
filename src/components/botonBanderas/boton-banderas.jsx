243e3589bd06a61299671c144aaf93d0bf91393c
Issblann <anaisabelpp@outlook.com>
2024-11-01 20:46:57 -0500

    agrego funciones para el manejo de la data al responder

--- src/components/botonBanderas/boton-banderas.jsx
+++ src/components/botonBanderas/boton-banderas.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
 import '../../styles/botonBanderas/styles.css';
+import { thunks } from '../../redux/slice/trivia/thunks';
+import { actions } from '../../redux/slice/trivia/slice';
 const BotonBanderas = () => {
-  return (
-    <section className="boton-banderas__seccion">
-      <button className="button-banderas">EE.UU. ISLAS PERIFÉRICAS</button>
+  const dispatch = useDispatch();
+  const { status, error, currentCountry, incorrectOption, showModal, score } =
+    useSelector((state) => state.trivia);
 
-      <button className="button-banderas">PAISES BAJOS</button>
-    </section>
+  useEffect(() => {
+    dispatch(thunks.fetchCountryNames()).then(() => {
+      dispatch(thunks.fetchRandomCountries());
+    });
+  }, [dispatch]);
+
+  const handleNextQuestion = () => {
+    dispatch(thunks.fetchRandomCountries());
+  };
+
+  const handleAnswer = (selectedOption) => {
+    dispatch(actions.checkAnswer(selectedOption));
+    if (!showModal) {
+      handleNextQuestion();
+    }
+  };
+
+  // console.log(currentCountry, 'pais correcto');
+  // console.log(incorrectOption, 'pais incorrecto');
+  console.log(score, 'puntaje');
+  console.log(showModal, 'modal');
+  return (
+    <>
+      {status === 'loading' && <p>Cargando...</p>}
+      {error && <p>Error: {error}</p>}
+      {currentCountry && incorrectOption && (
+        <section className="boton-banderas__seccion">
+          <button
+            onClick={() => handleAnswer(currentCountry.name)}
+            className="button-banderas"
+          >
+            {currentCountry.name}
+          </button>
+
+          <button
+            onClick={() => handleAnswer(incorrectOption.name)}
+            className="button-banderas"
+          >
+            {incorrectOption.name}
+          </button>
+        </section>
+      )}
+    </>
   );
 };
 
