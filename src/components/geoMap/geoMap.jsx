83327a748dbde407be863b9ceb9510db754e64e0
Issblann <anaisabelpp@outlook.com>
2024-11-04 21:42:41 -0500

    geo map con grafico de mapa y tooltip

---
+++ src/components/geoMap/geoMap.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import MapChart from './mapChart';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import '../../styles/geoMap/styles.css';
+const GeoMap = () => {
+  const [content, setContent] = useState('');
+  console.log(content);
+  return (
+    <div>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip
+        id="tooltip"
+        style={{
+          backgroundColor: '#181c1d',
+          color: '#fff',
+          borderRadius: '10px',
+        }}
+        // place="top"
+        float
+        // positionStrategy="fixed"
+        content={content}
+      />
+    </div>
+  );
+};
+
+export default GeoMap;
---
+++ src/components/geoMap/mapChart.jsx
@@ -0,0 +1,62 @@
+import React, { memo } from 'react';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Graticule,
+  Sphere,
+  ZoomableGroup,
+} from 'react-simple-maps';
+const MapChart = ({ setTooltipContent }) => {
+  return (
+    <div>
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147,
+        }}
+      >
+        <ZoomableGroup>
+          <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+          <Graticule stroke="#92cace" />
+          <Geographies geography="../../../public/JSOMAP/features.json">
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  data-tooltip-id="tooltip"
+                  data-tooltip-content={geo.properties.name}
+                  data-tooltip-float={true}
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    setTooltipContent(`${geo.properties.name}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent('');
+                  }}
+                  fill="#356169"
+                  style={{
+                    default: {
+                      fill: '#92cace',
+                      outline: 'none',
+                    },
+                    hover: {
+                      fill: '#356169',
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#356169',
+                      outline: 'none',
+                    },
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>
+  );
+};
+
+export default memo(MapChart);
