65a78dd83e6474e18493c3f0cd68940b019c6ae9
Nathalia Galvis <karolgalvis10@gmail.com>
2024-11-06 18:03:57 -0500

    Modificar ruta para uso especifico de fondo

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import GeoMap from '../components/geoMap/geoMap';
 import TriviaPage from '../components/trivia/TriviaPage';
 
 const AppRoutes = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.body.classList.add('background-trivia');
+    } else {
+      document.body.classList.remove('background-trivia');
+    }
+  }, [location]);
+
   return (
     <Routes>
       <Route path="/" element={<TriviaPage />} />
