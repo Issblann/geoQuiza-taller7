843cb2a98d9632e81cea38edbfdea7bbe537310b
Eri1607 <eri16061142@gmail.com>
2024-11-05 19:23:32 -0500

    Añadir estilos para estados correcto e incorrecto en botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import BotonBanderas from './components/botonBanderas/boton-banderas';
+import Card from './components/card/flags_card';
 import Layout from './layout/layout';
 
 function App() {
   return (
     <Layout>
+      <Card />
       <BotonBanderas />
     </Layout>
   );
--- src/components/botonBanderas/boton-banderas.jsx
+++ src/components/botonBanderas/boton-banderas.jsx
@@ -1,12 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/botonBanderas/styles.css';
 import { thunks } from '../../redux/slice/trivia/thunks';
 import { actions } from '../../redux/slice/trivia/slice';
+
 const BotonBanderas = () => {
   const dispatch = useDispatch();
-  const { status, error, currentCountry, incorrectOption, showModal, score } =
-    useSelector((state) => state.trivia);
+  const { status, error, currentCountry, incorrectOption, showModal, score } = useSelector((state) => state.trivia);
+  const [answerStatus, setAnswerStatus] = useState(null); // Nuevo estado
 
   useEffect(() => {
     dispatch(thunks.fetchCountryNames()).then(() => {
@@ -15,20 +16,20 @@ const BotonBanderas = () => {
   }, [dispatch]);
 
   const handleNextQuestion = () => {
+    setAnswerStatus(null); // Restablece el estado al cambiar la pregunta
     dispatch(thunks.fetchRandomCountries());
   };
 
   const handleAnswer = (selectedOption) => {
+    const isCorrect = selectedOption === currentCountry.name;
+    setAnswerStatus(isCorrect ? 'correct' : 'incorrect');
     dispatch(actions.checkAnswer(selectedOption));
+
     if (!showModal) {
-      handleNextQuestion();
+      setTimeout(handleNextQuestion, 1000); // Espera antes de mostrar la siguiente pregunta
     }
   };
 
-  // console.log(currentCountry, 'pais correcto');
-  // console.log(incorrectOption, 'pais incorrecto');
-  console.log(score, 'puntaje');
-  console.log(showModal, 'modal');
   return (
     <>
       {status === 'loading' && <p>Cargando...</p>}
@@ -37,14 +38,13 @@ const BotonBanderas = () => {
         <section className="boton-banderas__seccion">
           <button
             onClick={() => handleAnswer(currentCountry.name)}
-            className="button-banderas"
+            className={`button-banderas ${answerStatus === 'correct' ? 'correct' : ''}`}
           >
             {currentCountry.name}
           </button>
-
           <button
             onClick={() => handleAnswer(incorrectOption.name)}
-            className="button-banderas"
+            className={`button-banderas ${answerStatus === 'incorrect' ? 'incorrect' : ''}`}
           >
             {incorrectOption.name}
           </button>
