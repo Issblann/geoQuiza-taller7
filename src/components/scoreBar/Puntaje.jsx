f9516387fcf2d752f846c70dc33f6c0998af42e2
Issblann <anaisabelpp@outlook.com>
2024-11-06 19:53:03 -0500

    arreglo de organizacion carpetas y puntaje desde store

--- src/components/scoreBar/Puntaje.jsx
+++ src/components/scoreBar/Puntaje.jsx
@@ -1,12 +1,12 @@
 import { useSelector } from 'react-redux';
-
+import '../../styles/scoreBar/styles.css';
 const Puntaje = () => {
   const { score } = useSelector((state) => state.trivia);
 
   return (
-    <div className="Puntaje">
-      <h2>Score: 100</h2>
-    </div>
+    <p className="text-gray-400 font-bold score w-full text-2xl">
+      PUNTOS {score}/261
+    </p>
   );
 };
 
